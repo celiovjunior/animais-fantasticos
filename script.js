dd8e8d58732ccc98f919341f6fc5ed20cf974a64
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2021-08-14 22:17:55 -0300

    modules and event edition

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ import initTabNav from "./modules/tabnav.js";
 import initAccordion from "./modules/accordion.js";
 import initScroll from "./modules/scroll-anima.js";
 import initScrollAnima from "./modules/scroll.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+import outsideClick from "./modules/outside-click.js";
 
 initTabNav();
 initAccordion();
 initScroll();
 initScrollAnima();
+initModal();
+initTooltip();
+initDropdownMenu();
+outsideClick();
