aec951217f62d467a136355d8bf2fb66114b759e
Celio Junior <celio_cjr@hotmail.com>
2024-07-24 23:03:39 +0930

    feat(function): created method onMouseLeave and onMouseMove to improve the tooltip behavior

--- js/module/tooltip.js
+++ js/module/tooltip.js
@@ -7,8 +7,32 @@ export default function initTooltip() {
 
     function onMouseOver(event) {
         const tooltipBox = createTooltipBox(this);
+
         tooltipBox.style.top = event.pageY + "px";
         tooltipBox.style.left = event.pageX + "px";
+
+        onMouseMove.tooltipBox = tooltipBox;
+        this.addEventListener('mousemove', onMouseMove);
+
+        onMouseLeave.tooltipBox = tooltipBox;
+        onMouseLeave.element = this;
+
+        this.addEventListener('mouseleave', onMouseLeave);
+    }
+
+    const onMouseLeave = {
+        handleEvent() {
+            this.tooltipBox.remove();
+            this.element.removeEventListener('mouseleave', onMouseLeave);
+            this.element.removeEventListener('mousemove', onMouseMove);
+        }
+    }
+
+    const onMouseMove = {
+        handleEvent(event) {
+            this.tooltipBox.style.top = event.pageY + 20 + "px";
+            this.tooltipBox.style.left = event.pageX + 20 + "px";
+        }
     }
 
     function createTooltipBox(element) {
